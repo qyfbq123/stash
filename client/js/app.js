acf7c7c5cbd5d98bf4e899d399fe5a05adb25b8c
BiteBit <kfbyj@gmail.com>
2015-09-20 21:18:54 +0800

    first commit

---
+++ client/js/app.js
@@ -0,0 +1,61 @@
+var app = angular.module('stashApp', ['ui.router', 'homeModule', 'loginModule', 'authModule', 'LocalStorageModule']);
+
+app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
+  $stateProvider.state('login', {
+    url: '/login',
+    templateUrl: './views/login.html',
+    controller: 'loginCtrl'
+  })
+
+  $stateProvider.state('logout', {
+    url: '/logout',
+    template:'<p> logout',
+    controller: function ($state, Auth) {
+      Auth.logout();
+      $state.go('login');
+    }
+  })
+
+  $stateProvider.state('home', {
+    url: '/home',
+    templateUrl: './views/home.html',
+    controller: 'homeCtrl'
+  })
+  .state('home.userManagement', {
+    url: '/userManagement',
+    templateUrl: './views/userManagement.html'
+  })
+  .state('home.inReport', {
+    url: '/inReport',
+    templateUrl: './views/inReport.html'
+  })
+  .state('home.roleManagement', {
+    url: '/roleManagement',
+    templateUrl: './views/roleManagement.html'
+  })
+  .state('home.outReport', {
+    url: '/outReport',
+    templateUrl: './views/outReport.html'
+  })
+
+  $urlRouterProvider.otherwise('/home');
+}]);
+
+
+// angular http请求默认是在payload中
+app.config(function($httpProvider){
+  $httpProvider.defaults.transformRequest = function(obj){
+    var str = [];
+    for(var p in obj) {
+       str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+    }
+    return str.join("&");
+  }
+  $httpProvider.defaults.headers.post = {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+});
+
+app.config(function (localStorageServiceProvider) {
+  localStorageServiceProvider.setPrefix('stash')
+});
---
+++ client/js/servs/auth.js
@@ -0,0 +1,39 @@
+
+
+angular.module('authModule', ['ngCookies', 'LocalStorageModule']).factory('Auth', ['$http', '$rootScope', '$state', 'localStorageService',
+function($http, $rootScope, $state, localStorageService) {
+  var host = $rootScope.apiHost = 'http://192.168.1.3:8080/';
+
+  return {
+    login: function (data) {
+      $('html').loading({message:'正在登录...'});
+
+      $http.post(host + 'mywms/main/login', data)
+      .success(function (data, status) {
+        $('html').loading('stop');
+
+        if (parseInt(data.status) != 0) {
+          $.messager.alert('登陆失败', data.message, 'error');
+        } else {
+          localStorageService.set('logined', true);
+          localStorageService.set('user', data.data);
+          $state.go('home');
+        }
+      })
+      .error(function (data, status) {
+        $('html').loading('stop');
+        $.messager.alert('登陆失败', 'http error:' + status + ' ' + data, 'error');
+      })
+    },
+    logout: function () {
+      localStorageService.set('logined', false);
+      localStorageService.remove('user');
+    },
+    logined:function () {
+      return Boolean(localStorageService.get('logined'));
+    },
+    user: function () {
+      return localStorageService.get('user');
+    },
+  }
+}]);
